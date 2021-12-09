9a079fb04c614755847133174a728bd9b8ee3714
lizihang <lzhsmart@163.com>
2021-12-09 17:41:01 +0800

    测试redis

--- dm-run/dm-run-service/src/test/java/com/dm/DmServiceApplicationTest.java
+++ dm-run/dm-run-service/src/test/java/com/dm/DmServiceApplicationTest.java
@@ -3,15 +3,13 @@ package com.dm;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.Set;
 /**
  * <p>标题：</p>
  * <p>功能：</p>
@@ -30,13 +28,50 @@ import java.util.concurrent.TimeUnit;
 public class DmServiceApplicationTest
 {
 	@Resource
-	RedisTemplate redisTemplate;
+	RedisTemplate<String,String> redisTemplate;
 
 	@Test
-	public void testList()
+	public void testRedisHash()
 	{
-		LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(10);
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 30, TimeUnit.SECONDS, queue);
-		ListOperations listOperations = redisTemplate.opsForList();
+		HashOperations<String,String,String> opsForHash = redisTemplate.opsForHash();
+		opsForHash.put("dm_user", "user_id_1", "value_1");
+		opsForHash.put("dm_user", "user_id_2", "value_2");
+		opsForHash.put("dm_user", "user_id_3", "value_3");
+		opsForHash.put("dm_user", "user_id_4", "value_4");
+		opsForHash.put("dm_user", "user_id_5", "value_5");
+		Set<String> keys = opsForHash.keys("dm_user");
+		for (String key : keys)
+		{
+			System.out.println("key:" + key);
+		}
+		Object v1 = opsForHash.get("dm_user", "user_id_1");
+		System.out.println("v1:" + v1);
+	}
+
+	@Test
+	public void testRedisHash2()
+	{
+		HashOperations<String,String,String> opsForHash = redisTemplate.opsForHash();
+		opsForHash.put("dm_user", "user_id_1", "new_value_1");
+	}
+
+	@Test
+	public void testRedisListPush()
+	{
+		ListOperations<String,String> opsForList = redisTemplate.opsForList();
+		for (int i = 0; i < 5; i++)
+		{
+			opsForList.leftPush("k1", "v" + i);
+		}
+	}
+
+	@Test
+	public void testRedisListPop()
+	{
+		ListOperations<String,String> opsForList = redisTemplate.opsForList();
+		for (int i = 0; i < 5; i++)
+		{
+			System.out.println(opsForList.leftPop("k1"));
+		}
 	}
 }
